098eb4d597cb42662e65bbd74e7c4a9614564365
JuanDiego <juandiegp17@gmail.com>
2018-09-28 17:40:19 -0400

    making better implementation on exercise of spoj

--- spoj/ALPHACODE.cpp
+++ spoj/ALPHACODE.cpp
@@ -1,41 +1,37 @@
 # include <bits/stdc++.h>
 using namespace std;
 
-void count_opt(size_t p, string  & s, size_t n, unordered_map<size_t, size_t> & m){
-	
-	if(p == n - 1){
-		if(s[p] == '0')
-			m[p] = 0;
-		else
-			m[p] = 1;
-		return;
-	}
-	
-	count_opt(p + 1, s, n, m);
-
-	if(p + 1 != n and (s[p] == '1' or s[p] == '2' and s[p + 1] <= '6')){
-		if(p + 2 != n and s[p + 2] == '0')
-			m[p] = m[p + 1];
-		else if(s[p + 1] == '0')
-			m[p] = m[p + 2];
-		else
-			m[p] = m[p + 1] + m[p + 2];
-	}
+void count(string s, size_t n, unsigned int m []){
+	m[n] = 1;
+	if(s[n - 1] == '0')
+		m[n - 1] = 0;
 	else
-		m[p] = m[p + 1];
-	return;
+		m[n - 1] = 1;
+	
+	for (int p = n - 1; p >= 0; p--){
+		if(p + 1 != n and (s[p] == '1' or s[p] == '2' and s[p + 1] <= '6')){
+			if(p + 2 != n and s[p + 2] == '0')
+				m[p] = m[p + 1];
+			else if(s[p + 1] == '0')
+				m[p] = m[p + 2];
+			else
+				m[p] = m[p + 1] + m[p + 2];
+		}
+		else
+			m[p] = m[p + 1];
+	}
 }
 
 int main (){
-	
+	size_t n;
 	string s;
 	
 	while(cin >> s){
 		if(s == "0")
 			break;
-		unordered_map<size_t, size_t> m;
-		m[s.length()] = 1;
-		count_opt(0, s, s.length(), m);
+		n = s.length();
+		unsigned int m[n + 1];
+		count(s, n, m);
 		cout << m[0] << endl;
 	}
 	return 0;
