c83d4f2ff704b67d89c7c8347d1475f09e3f8b4d
JuanDiego <juandiegp17@gmail.com>
2019-02-20 10:26:04 -0400

    adding

--- codeforces/1118C.cpp
+++ codeforces/1118C.cpp
@@ -1,85 +1,100 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
-# define endl "\n"
-# define io_boost std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-typedef unsigned long long int ulli;
-typedef long long int lli;
-typedef unsigned int ui;
-typedef vector<int> vector_i;
-typedef vector<long long int> vector_lli;
-typedef vector<unsigned int> vector_ui;
-typedef vector<unsigned long long int> vector_ulli;
-typedef set<int> set_i;
-typedef set<long long int> set_lli;
-typedef set<unsigned int> set_ui;
-typedef set<unsigned long long int> set_ulli;
-typedef unordered_set<int> u_set_i;
-typedef unordered_set<long long int> u_set_lli;
-typedef unordered_set<unsigned int> u_set_ui;
-typedef unordered_set<unsigned long long int> u_set_ulli;
-
-
-const int N = 1010;
-
-int freq[N];
-int a[N];
-
-int odd = 0 , even = 0;
-int n;
-
-set < int > st;
-
-void no(){
-    printf("NO\n");
-    exit(0);
-}
-
-int CNT(int i , int j){
-    int ret = 1;
-    if(n-i+1 != i)ret ++;
-    if(n-j+1 != j)ret ++;
-    if(n-i+1 != i && n-j+1 != j)ret ++;
-    return ret;
-}
-
-int ans[N][N];
-
-main(){
-    scanf("%d" , &n);
-
-    for(int i = 1 ; i <= n*n ; i ++){
-        scanf("%d" , &a[i]);
-        freq[ a[i] ] ++;
-        st.insert(a[i]);
+int main(){
+    int n;
+    cin >> n;
+    int a[n*n], f[1001]={0};
+    for(int i=0; i<n*n; i++){
+        cin >> a[i];
+        f[a[i]]++;
     }
 
-    for(int i = 1 ; i <= (n+1)/2 ; i ++){
-        for(int j = 1 ; j <= (n+1)/2 ; j ++){
+    multiset<int> s1,s2,s3;
 
-            int positions = CNT(i , j);
-
-            int x = 0;
-            for(auto S : st)
-                if(freq[S] >= positions){
-                    x = S;
-                    break;
-                }
-            if(x == 0){
-                no();
+    for(int i=1; i<=1000; i++){
+        if(f[i]%4==1 || f[i]%4==3){
+            s3.insert(i);
+            f[i]--;
+        }
+        if(f[i]%4==2){
+            s2.insert(i);
+            f[i] -= 2;
+        }
+        if (f[i] % 4 == 0) {
+            while (f[i]) {
+                s1.insert(i);
+                f[i] -= 4;
             }
-            ans[i][j]          = x;
-            ans[n-i+1][j]      = x;
-            ans[i][n-j+1]      = x;
-            ans[n-i+1][n-j+1]  = x;
-
-            freq[x] -= positions;
-
         }
     }
 
-    printf("YES\n");
-    for(int i = 1 ; i <= n ; i ++)
-        for(int j = 1 ; j <= n ; j ++)
-            printf("%d%c" , ans[i][j] , (j == n ? '\n' : ' '));
+    int m[n][n]={0};
+    for(int i=0; i<n/2; i++){
+        for(int j=0; j<n/2; j++){
+            if(s1.size()==0){
+                cout << "NO" << endl;
+                return 0;
+            }
+            int cur = *s1.begin();
+            s1.erase(s1.begin());
+            m[i][j] = cur;
+            m[n-i-1][j] = cur;
+            m[i][n-j-1] = cur;
+            m[n-i-1][n-j-1] = cur;
+        }
+    }
+
+    if(n%2==1){ 
+        while(s1.size()){
+            s2.insert(*s1.begin());
+            s2.insert(*s1.begin());
+            s1.erase(s1.begin());
+        }
+
+
+        for(int i=0; i<n/2; i++){
+                int j=n/2;
+                if(s2.size()==0){
+                    cout << "NO" << endl;
+                    return 0;
+                }
+                int cur = *s2.begin();
+                s2.erase(s2.begin());
+                m[i][j] = cur;
+                m[n-i-1][j] = cur;
+        }
+
+
+        for(int j=0; j<n/2; j++){
+                int i=n/2;
+                if(s2.size()==0){
+                    cout << "NO" << endl;
+                    return 0;
+                }
+                int cur = *s2.begin();
+                s2.erase(s2.begin());
+                m[i][j] = cur;
+                m[i][n-j-1] = cur;
+        }
+
+        if(s3.size()==0){
+            cout << "NO" << endl;
+            return 0;
+        }
+        m[n/2][n/2] = *s3.begin();
+    }
+
+    cout << "YES" << endl;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            cout << m[i][j] << " " ;
+        }
+        cout << endl;
+    }
+
+
+
+    return 0;
 }
