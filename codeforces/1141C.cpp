e712d55078318f8f15f057505606ff593ddf648c
JuanDiego <juandiegp17@gmail.com>
2019-03-20 10:55:28 -0400

    changing some shits

--- codeforces/1141C.cpp
+++ codeforces/1141C.cpp
@@ -19,27 +19,27 @@ typedef unordered_set<long long int> u_set_lli;
 typedef unordered_set<unsigned int> u_set_ui;
 typedef unordered_set<unsigned long long int> u_set_ulli;
 
+const int MAX = 2e5+5;
+long long q[MAX], p[MAX] = {0};
+int m[MAX] = {false};
 int main() {
 	io_boost;
 	size_t n;
 	cin >> n;
-	int q[n - 1];
+	
 	for (size_t i = 0; i < n - 1; ++i) cin >> q[i];
-	long long p[n] = {0};
+	
 	long long x = 0;
 	for (int i = 1; i < n; ++i){
 		p[i] = p[i-1] + q[i-1]; 
 		x = min(x, p[i]);
 	}
-	bool m[n] = {false};
+	
 	for (int i = 0; i < n; ++i){
 		p[i] -= x; 
-		if(m[p[i]] or p[i]  + 1> n){
-			cout << -1 << endl;
-			return 0;
-		}
-		m[p[i]] = true;
+		if(p[i] < n) m[p[i]]++;
 	}
+	for (int i = 0; i < n; ++i){if(m[i] != 1){cout << -1 << endl; return 0;}} 
 	for (int i = 0; i < n; ++i) cout << p[i] + 1 << " ";
 	cout << endl;
     return 0;
