463aa3481def520d27192d11ea7ec4013a044a78
JuanDiego <juandiegp17@gmail.com>
2019-05-14 13:09:10 -0400

    adding useful function

--- library/maths/math.cpp
+++ library/maths/math.cpp
@@ -76,6 +76,18 @@ int nCr_mod_M(int n, int r, int MOD){
     return C[r]; 
 } 
 
+//############################################## Print All Divisors #################################################
+void printDivisors(int n){  
+    for (int i=1; i<=sqrt(n); i++){ 
+        if (n%i == 0){ 
+            if (n/i == i) 
+                printf("%d ", i); 
+            else
+                printf("%d %d ", i, n/i); 
+        } 
+    } 
+} 
+
 int main(){
 
 	return 0;
