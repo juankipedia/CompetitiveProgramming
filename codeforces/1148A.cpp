a1b786577582304a0f69a294e126f6f405ffbe29
JuanDiego <juandiegp17@gmail.com>
2019-06-01 19:43:39 -0400

    adding

---
+++ codeforces/1148A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+# define endl "\n"
+# define io_boost std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+typedef unsigned long long int ulli;
+typedef long long int lli;
+typedef unsigned int ui;
+typedef vector<int> vector_i;
+typedef vector<long long int> vector_lli;
+typedef vector<unsigned int> vector_ui;
+typedef vector<unsigned long long int> vector_ulli;
+typedef set<int> set_i;
+typedef set<long long int> set_lli;
+typedef set<unsigned int> set_ui;
+typedef set<unsigned long long int> set_ulli;
+typedef unordered_set<int> u_set_i;
+typedef unordered_set<long long int> u_set_lli;
+typedef unordered_set<unsigned int> u_set_ui;
+typedef unordered_set<unsigned long long int> u_set_ulli;
+typedef std::numeric_limits< double > dbl;
+
+ulli a, b ,c, r;
+
+
+int main() {
+	cin >> a >> b >> c;
+	r = c * 2;
+	if(a > b){
+		r += b*2;
+		r ++;
+	}
+	else if (b > a){
+		r += a*2;
+		r ++;
+	}
+	else r += a*2;
+
+	cout << r << endl;
+    return 0;
+}
