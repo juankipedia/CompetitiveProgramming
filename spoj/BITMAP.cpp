5c2219d1ff9eda358124feffaee4a96b3ba3fc32
juan <juandiegp17@gmail.com>
2020-03-30 09:32:47 -0400

    updating problem

--- spoj/BITMAP.cpp
+++ spoj/BITMAP.cpp
@@ -1,105 +1,77 @@
-#include<cstdio>
-#include<iostream>
-#include<string>
-#include<cstdlib>
-#include<queue>
-#include<stack>
-#include<utility>
-#include<string>
-#include<cstring>
-#include<set>
-#include<vector>
-#include<fstream>
-#include<map>
-#include<list>
-#include<algorithm>
-#define MAX 10000000
-#define in_range(x, y, r, c) (x >= 0 && x < r && y >= 0 && y < c)
-
+#include <bits/stdc++.h>
 using namespace std;
 
-queue<int> myq;
+# define endl "\n"
+# define io_boost std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+typedef unsigned long long int ulli;
+typedef long long int lli;
+typedef unsigned int ui;
+typedef vector<int> vector_i;
+typedef vector<long long int> vector_lli;
+typedef vector<unsigned int> vector_ui;
+typedef vector<unsigned long long int> vector_ulli;
+typedef set<int> set_i;
+typedef set<long long int> set_lli;
+typedef set<unsigned int> set_ui;
+typedef set<unsigned long long int> set_ulli;
+typedef unordered_set<int> u_set_i;
+typedef unordered_set<long long int> u_set_lli;
+typedef unordered_set<unsigned int> u_set_ui;
+typedef unordered_set<unsigned long long int> u_set_ulli;
+const int INF = 1000;
 
-void bfs(char mat[182][182], int dis[182][182], int x, int y, int r, int c){
-	
-	int i, j, a, b;
-	
-	dis[x][y] = 0;
-	myq.push(x);
-	myq.push(y);
-	
-	while(!myq.empty()){
-		
-		i = myq.front();
-		myq.pop();
-		j = myq.front();
-		myq.pop();
-		
-		a = i-1;
-		b = j;
-		
-		if(in_range(a, b, r, c) && dis[a][b] > dis[i][j] + 1 && mat[a][b] == '0'){
-			myq.push(a);
-			myq.push(b);
-			dis[a][b] = dis[i][j] + 1;
-		}
-		
-		a = i+1;
-		b = j;
-		
-		if(in_range(a, b, r, c) && dis[a][b] > dis[i][j] + 1 && mat[a][b] == '0'){
-			myq.push(a);
-			myq.push(b);
-			dis[a][b] = dis[i][j] + 1;
-		}
-		
-		a = i;
-		b = j-1;
-		
-		if(in_range(a, b, r, c) && dis[a][b] > dis[i][j] + 1 && mat[a][b] == '0'){
-			myq.push(a);
-			myq.push(b);
-			dis[a][b] = dis[i][j] + 1;
-		}
-		
-		a = i;
-		b = j+1;
-		
-		if(in_range(a, b, r, c) && dis[a][b] > dis[i][j] + 1 && mat[a][b] == '0'){
-			myq.push(a);
-			myq.push(b);
-			dis[a][b] = dis[i][j] + 1;
-		}
-		
-	}
+int n, m, t;
+int index_i[] = {1, -1, 0, 0}, index_j[] = {0, 0, 1, -1};
+queue<int> x;
+
+void bfs(int i, int j, int d[182][182], char g[182][182]){
+
+    int ii, jj, ni, nj;
+
+    d[i][j] = 0;
+    x.push(i);
+    x.push(j);
+
+    while(!x.empty()){
+        ni = x.front();
+        x.pop();
+        nj = x.front();
+        x.pop();
+        for(int l = 0; l < 4; l++){
+            ii = ni + index_i[l];
+            jj = nj + index_j[l];
+            if(ii >= 0 && ii < n && jj >= 0 && jj < m && (1 + d[ni][nj]) < d[ii][jj] && g[ii][jj] == '0'){
+                d[ii][jj] = 1 + d[ni][nj];
+                x.push(ii);
+                x.push(jj);
+            }
+        }
+    }
 }
 
-int main(){
-	
-	char mat[182][182], ch;
-	int t, r, c, dis[182][182];
-	scanf("%d", &t);
-	while(t--){
-		scanf("%d%d%c", &r, &c, &ch);
-		for(int i=0;i<r;i++){
-			for(int j=0;j<c;j++){
-				scanf("%c", &mat[i][j]);
-				dis[i][j] = MAX;
-			}
-			scanf("%c", &ch);
-		}
-		for(int i=0;i<r;i++)
-			for(int j=0;j<c;j++){
-				if(mat[i][j] == '1')
-					bfs(mat, dis, i, j, r, c);
-			}
-			
-		for(int i=0;i<r;i++){
-			for(int j=0;j<c;j++)
-				printf("%d ", dis[i][j]);
-			printf("\n");
-		}
-	}
-	
+int main() {
+    int d[182][182];
+    char g[182][182], ch;
+
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%c", &n, &m, &ch);
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                scanf("%c", &g[i][j]);
+                d[i][j] = INF;
+            }
+            scanf("%c", &ch);
+        }
+
+        for (int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++)
+                if(g[i][j] == '1') bfs(i, j, d, g);
+
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++) printf("%d ", d[i][j]);
+             printf("\n");
+        }
+    }
     return 0;
 }
