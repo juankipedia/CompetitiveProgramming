9729f551e994d69ed4356b7a19e56540bae90a7c
juan <juandiegp17@gmail.com>
2020-05-05 14:12:11 -0400

    better implementation

--- spoj/PT07Y.cpp
+++ spoj/PT07Y.cpp
@@ -1,70 +1,34 @@
-#include <bits/stdc++.h>
+# include <bits/stdc++.h>
+ 
 using namespace std;
-
-# define endl "\n"
-# define io_boost std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
-typedef unsigned long long int ulli;
-typedef long long int lli;
-typedef unsigned int ui;
-typedef vector<int> vector_i;
-typedef vector<long long int> vector_lli;
-typedef vector<unsigned int> vector_ui;
-typedef vector<unsigned long long int> vector_ulli;
-typedef set<int> set_i;
-typedef set<long long int> set_lli;
-typedef set<unsigned int> set_ui;
-typedef set<unsigned long long int> set_ulli;
-typedef unordered_set<int> u_set_i;
-typedef unordered_set<long long int> u_set_lli;
-typedef unordered_set<unsigned int> u_set_ui;
-typedef unordered_set<unsigned long long int> u_set_ulli;
-
-const int MAXN = 10000;
-const int MAXM = 20000;
-int n, m, s, t;
-
-vector<int> G[MAXN];
-bool visited[MAXN];
-
-bool _dfs(int v, int p,  int &c){
-	if(visited[v])
-		return false;
-	visited[v] = true;
-	c++;
-	for (const auto & vi: G[v]){
-		if(vi != p and not _dfs(vi, v, c)){
-			return false;
-		}
-	}
-
-	return true;
+int n, m, u, v;
+ 
+void dfs(int i, bool v[], vector<int> G[]){
+    if(v[i]) return;
+    v[i] = true;
+    for(const int &u: G[i]) dfs(u, v, G);
 }
-
-bool dfs(){
-	int c = 0;
-	if(_dfs(0, -1, c) and c == n)
-		return true;
-	else return false;
+ 
+int f(vector<int> G[]){
+    bool v[n + 1] = {false};
+    dfs(1, v, G);
+    for(int i = 1; i <= n; i++) if(!v[i]) return false;
+    return true;
 }
-
-int main() {
-	cin >> n >> m;
-
-	for (int i = 0; i < m; ++i){
-		cin >> s >> t;
-		G[s - 1].push_back(t - 1);
-		G[t - 1].push_back(s - 1);
-	}
-
-	if(n <= m){
-		cout << "NO" << endl;
-		return 0;
-	}
-	
-	if(dfs()){
-		cout << "YES" << endl;
-		return 0;
-	}
-	cout << "NO" << endl;
+ 
+int main(){
+    cin >> n >> m;
+    vector<int> G[n + 1];
+    for(int i = 0; i < m; i++){
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+    }
+    if((n - 1) != m) cout << "NO" << endl;
+    else{
+        if(!f(G)) cout << "NO" << endl;
+        else cout << "YES" << endl; 
+    }
+        
     return 0;
-}
+} 
