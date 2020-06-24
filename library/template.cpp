1bdb70cbf5bb3952f1b99b6dcdb4968baa371363
juan <juandiegp17@gmail.com>
2020-06-24 01:08:52 -0400

    simpler template

--- library/template.cpp
+++ library/template.cpp
@@ -6,20 +6,9 @@ using namespace std;
 typedef unsigned long long int ulli;
 typedef long long int lli;
 typedef unsigned int ui;
-typedef vector<int> vector_i;
-typedef vector<long long int> vector_lli;
-typedef vector<unsigned int> vector_ui;
-typedef vector<unsigned long long int> vector_ulli;
-typedef set<int> set_i;
-typedef set<long long int> set_lli;
-typedef set<unsigned int> set_ui;
-typedef set<unsigned long long int> set_ulli;
-typedef unordered_set<int> u_set_i;
-typedef unordered_set<long long int> u_set_lli;
-typedef unordered_set<unsigned int> u_set_ui;
-typedef unordered_set<unsigned long long int> u_set_ulli;
 
-int main() {
+
+int main(){
 
     return 0;
 }
