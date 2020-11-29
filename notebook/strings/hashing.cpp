e0073e5812916607eae96ae625197c35924ef974
juan <juandiegp17@gmail.com>
2020-11-29 19:49:21 -0400

    adding

--- notebook/strings/hashing.cpp
+++ notebook/strings/hashing.cpp
@@ -21,7 +21,7 @@ ulli MODS[] = {1000000007, 10657331232548839, 790738119649411319};
 */
 
 // Using single variable.
-ulli rolling_hash(string &&s, ulli b, ulli p){
+ulli rolling_hash(string &s, ulli b, ulli p){
     //hash(s) = sum(s[i] * b ^ (n - 1 - i)) % p, for all i in [i, n = s.size()]
     ulli hash = (s[0] - 'a' + 1);
     for(int i = 1; i < s.size(); i++)
@@ -31,7 +31,7 @@ ulli rolling_hash(string &&s, ulli b, ulli p){
 
 // Using array. Useful for getting hash of a subsegment, complete hash at get_hash(0, s.size() - 1)
 // hash stored in h[] and B ^ i = b[i]
-void rolling_hash(string &&s, lli h[], lli b[], lli B, lli p){
+void rolling_hash(string &s, lli h[], lli b[], lli B, lli p){
     h[0] = 0;
     h[1] = (s[0] - 'a' + 1);
     b[0] = 1;
