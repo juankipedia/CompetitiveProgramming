a046417c8e9c291670be9c0a9296f623fbf67ed8
juan <juandiegp17@gmail.com>
2021-01-02 18:24:40 -0400

    adding typedef

--- notebook/useful-algorithms/matrix-exponentiation.cpp
+++ notebook/useful-algorithms/matrix-exponentiation.cpp
@@ -21,8 +21,9 @@ typedef unsigned int ui;
  * 
  **/
 
+typedef vector<vector<ulli>> matrix;
 
-void multiply(vector<vector<ulli>> &m1, vector<vector<ulli>> &m2, vector<vector<ulli>> &res, ulli mod){
+void multiply(matrix &m1, matrix &m2, matrix &res, ulli mod){
     int N = m1.size(), K = m1[0].size(), M = m2[0].size();
     ulli a[N][K], b[K][M];
     for(int i = 0; i < N; i++) for(int j = 0; j < K; j++) a[i][j] = m1[i][j];
@@ -36,9 +37,9 @@ void multiply(vector<vector<ulli>> &m1, vector<vector<ulli>> &m2, vector<vector<
     }
 }
 
-void matrix_power(vector<vector<ulli>> &m, ulli r, ulli mod){
+void matrix_power(matrix &m, ulli r, ulli mod){
     int N = m.size();
-    vector<vector<ulli>> cur(N, vector<ulli>(N, 0));
+    matrix cur(N, vector<ulli>(N, 0));
     for(int i = 0; i < N; i++){
         for(int j = 0; j < N; j++){
             cur[i][j] = m[i][j];
@@ -56,7 +57,7 @@ void matrix_power(vector<vector<ulli>> &m, ulli r, ulli mod){
 ulli fibonacci(ulli n, ulli mod){
     if(n == 0) return 0;
     if(n == 1) return 1;
-    vector<vector<ulli>> init = {{0, 1}}, m = {{0, 1}, {1, 1}}, res = {{0, 0}};
+    matrix init = {{0, 1}}, m = {{0, 1}, {1, 1}}, res = {{0, 0}};
     matrix_power(m, n - 1, mod);
     multiply(init, m, res, mod);
     return res[0][1];
