484304dd44540553a7fc993c538d25beb13eea60
juan <juandiegp17@gmail.com>
2021-01-05 14:27:58 -0400

    solving

--- codeforces/1472B.cpp
+++ codeforces/1472B.cpp
@@ -8,36 +8,22 @@ typedef long long int lli;
 typedef unsigned int ui;
  
 int t, n, a[100];
- 
-const int MAX = 100; 
-int ispossible[MAX]; 
-void check(int arr[], int N) { 
-    memset(ispossible, 0, sizeof(ispossible));
-    ispossible[0] = 1; 
-    sort(arr, arr + N); 
-    for (int i = 0; i < N; ++i) { 
-        int val = arr[i];
-        if (ispossible[val]) 
-            continue;
-        for (int j = 0; j + val < MAX; ++j) 
-            if (ispossible[j]) 
-                ispossible[j + val] = 1; 
-    } 
-} 
+
 int main(){
     cin >> t;
     while(t--){
         cin >> n;
-        int sum = 0;
-        for(int i = 0; i < n; i++) cin >> a[i], sum += a[i];
-        if(sum % 2){
+        int sum = 0, cnt = 0;
+        for(int i = 0; i < n; i++){
+            cin >> a[i];
+            sum += a[i];
+            if(a[i] == 1) cnt ++;
+        }
+        if(sum % 2 || (sum / 2) % 2 && cnt == 0){
             cout << "NO" << endl;
             continue;
         }
-        sum /= 2;
-        check(a, n);
-        if(ispossible[sum]) cout << "YES" << endl;
-        else cout << "NO" << endl;
+        else cout << "YES" << endl;
     }
     return 0;
 }
