95574abb05416a6d9f95444cce1378beb9efd929
juan <juandiegp17@gmail.com>
2021-02-26 09:52:08 -0400

    solving minor issue

--- codeforces/1490F.cpp
+++ codeforces/1490F.cpp
@@ -1,14 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 # define endl "\n"
 # define io_boost std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 typedef unsigned long long int ulli;
 typedef long long int lli;
 typedef unsigned int ui;
-
+ 
 int t, a[200005], n;
-
+ 
 int main(){
     io_boost;
     cin >> t;
@@ -16,9 +16,12 @@ int main(){
         cin >> n;
         map<int, int> m;
         for(int i = 0; i < n; i++) cin >> a[i], m[a[i]]++;
-        set<int> f;
-        for(auto &p: m) f.insert(p.second);
-        f.insert(0);
+        
+        vector<int> f;
+        f.push_back(0);
+        for(auto &p: m) f.push_back(p.second);
+        f.resize(unique(f.begin(), f.end()) - f.begin());
+
         int ans = 1e9 + 5;
         for(int fq: f){
             int ansi = 0;
