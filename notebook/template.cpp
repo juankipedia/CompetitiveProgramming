031febeaa7bf127a5571b703edc1dbacbf64e2a8
juankipedia <juandiegp17@gmail.com>
2021-07-21 08:34:34 -0400

    better template

--- notebook/template.cpp
+++ notebook/template.cpp
@@ -5,7 +5,6 @@ using namespace std;
 # define io_boost std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 typedef unsigned long long int ulli;
 typedef long long int lli;
-typedef unsigned int ui;
 
 
 int main(){
