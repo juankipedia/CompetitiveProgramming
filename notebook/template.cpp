a5538bd679cebf2b4c5550b52869d77fce4ec8d1
juan <juandiegp17@gmail.com>
2021-07-30 10:05:03 -0400

    new beggining ?

--- notebook/template.cpp
+++ notebook/template.cpp
@@ -1,11 +1,11 @@
 # include <bits/stdc++.h>
 using namespace std;
-
+/*************************************************************************************/
 # define endl "\n"
 # define io_boost std::ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
 typedef unsigned long long int ulli;
 typedef long long int lli;
-
+/*************************************JUANKIPEDIA*************************************/
 
 int main(){
 
