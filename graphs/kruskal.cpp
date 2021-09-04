d4869c5097ad581aec2f4c385f910b38f89a49b4
juan <juandiegp17@gmail.com>
2021-09-04 14:26:26 -0400

    changing some things on implementations

--- graphs/kruskal.cpp
+++ graphs/kruskal.cpp
@@ -13,30 +13,39 @@ typedef pair<lli, pair<int, int>> edge;
 
 const int MAXM = 100000, MAXN = 100000;
 edge edges[MAXM], mst[MAXN];
-int N, M, p_set[MAXN];
+int N, M;
 
-void build(){
-    for(int i = 0; i < N; i++) p_set[i] = i;
-}
+struct DSU {
+	int n, comp = n;
+	vector<int> p, len;
+	DSU(int n) : n(n){
+		p.assign(n + 10, 0); 
+        iota(p.begin(), p.end(), 0);
+		len.assign(n + 10, 1);
+	}
+	int find(int n){ return p[n] = p[n] == n ? n : find(p[n]); }
+	void join(int a, int b){
+		a = find(a), b = find(b);
+		if(a == b) return;
+		if(len[a] < len[b]) swap(a, b);
+		len[a] += len[b];
+		p[b] = a;
+		comp--;
+	}
+};
 
-int find(int a){
-    if(p_set[a] != a) p_set[a] = find(p_set[a]);
-    return p_set[a];
-}
-
-void join(int a, int b){
-    p_set[find(a)] = find(b);
-}
-
-void kruskal(){
+lli kruskal(){
+    DSU dsu(N);
     sort(edges, edges + M);
-    build();
+    lli sum = 0;
     for(int i = 0, e = 0; i < N - 1;){
-        int u = find(U(edges[e])), v = find(V(edges[e]));
+        int u = dsu.find(U(edges[e])), v = dsu.find(V(edges[e]));
         if(u != v){
-            join(u, v);
+            dsu.join(u, v);
             mst[i++] = edges[e];
+            sum += W(edges[e]);
         }
         e++;
     }
+    return sum;
 }
