a4603f33547ad3114e71bf50ba2f118a474dc3f0
juankipedia <juandiegp17@gmail.com>
2022-10-15 11:45:57 -0500

    updating segment trees

--- data-structures/segment-tree-massive-lazy-propagation.cpp
+++ data-structures/segment-tree-massive-lazy-propagation.cpp
@@ -3,83 +3,37 @@
  * SEGMENT TREE FOR MASSIVE OPERATIONS WITH LAZY PROPAGATION
  * 
  *  Example:
- *  assign value v to all elements on the segment from l to r. (sta)
+ *  assign value v to all elements on the segment from l to r. (lazy)
  *  find the sum on the segment from l to r. (st)
  *  
- *  note: additional operation must be distributive.
+ *  note: lazy operation must be distributive.
  *  
- * REQUIRED:
- *      - set_children
- *      - operation
- *      - join
- *      - NEUTRAL
- * 
- *      For assignment and sum, (assignment is the additional operation):
- * 
- *          T NEUTRAL(){ return 0; }
- *          T join(T lval, T rval){
- *              return lval + rval;
- *          }         
- *          void operation(int ss, int se, int si){
- *              st[si] = (se - ss + 1) * sta[si];
- *          }
- *          void set_children(int ss, int se, int si, T val){
- *              if(ss == se) return;
- *              sta[L(si)] = sta[R(si)] = val;
- *              stb[L(si)] = stb[R(si)] = true;
- *          }
- *              
- *      For addition and minimum, (addition is the additional operation):
- * 
- *          T NEUTRAL(){ return oo; }
- *          T join(T lval, T rval){
- *              return min(lval, rval);
- *          }         
- *          void operation(int ss, int se, int si){
- *              st[si] += sta[si];
- *          }
- *          void set_children(int ss, int se, int si, T val){
- *              if(ss == se) return;
- *              sta[L(si)] += val;
- *              sta[R(si)] += val;
- *              stb[L(si)] = stb[R(si)] = true;
- *          }
- *
- * VARIABLES:
- *      st: segment tree.
- *      sta: lazy propagated segment tree for additional operations.
- *      stb: to know if a given node of the segment tree (sta) has an operation to be propagated.
- *      a: initial array of values.
- *   
- * */
+ **/
 
 template<class T> struct SegmentTree{
     
     int N;
-    vector<T> st, sta;
-    vector<bool> stb;
-
+    vector<T> st, lazy;
+    vector<bool> tag;
+ 
     int R(int si){ return 2 * si + 2; }
     int L(int si){ return 2 * si + 1; }
     int MID(int ss, int se){ return (se + ss) / 2; }
     T NEUTRAL(){ return 0; }
-
+ 
     SegmentTree(vector<T> &a) : N(a.size()){
         st.assign(4 * N, 0);
-        sta.assign(4 * N, 0);
-        stb.assign(4 * N, false);
+        lazy.assign(4 * N, 0);
+        tag.assign(4 * N, 0);
         build(a, 0, N - 1);
     }
 
     SegmentTree(int n) : N(n){
         st.assign(4 * N, 0);
-        sta.assign(4 * N, 0);
-        stb.assign(4 * N, false);
+        lazy.assign(4 * N, 0);
+        tag.assign(4 * N, 0);
     }
-
-    T join(T lval, T rval){
-    }
-
+ 
     void build(vector<T> &a, int ss, int se, int si = 0){
         if(ss == se){
             st[si] = a[ss];
@@ -87,45 +41,41 @@ template<class T> struct SegmentTree{
         }
         build(a, ss, MID(ss, se), L(si));
         build(a, MID(ss, se) + 1, se, R(si));
-        st[si] = join(st[L(si)], st[R(si)]); 
+        st[si] = st[L(si)] + st[R(si)]; 
     }
 
-    void operation(int ss, int se, int si){
+ 
+    void push_down(int ss, int se, int si){
+        if(tag[si]){
+            st[L(si)] = lazy[si] * (MID(ss, se) - ss + 1);
+            st[R(si)] = lazy[si] * (se - MID(ss, se));
+            lazy[L(si)] = lazy[R(si)] = lazy[si];
+            tag[L(si)] = tag[R(si)] = 1;
+            tag[si] = 0;
+        }
     }
-
-    void set_children(int ss, int se, int si, T val){
-        if(ss == se) return;
-        //set children
-        stb[L(si)] = stb[R(si)] = true;
-    }
-    void propagate(int ss, int se, int si){
-        if(!stb[si]) return;
-        operation(ss, se, si); // sets value for st[si] using value in sta[si].
-        stb[si] = false;
-        set_children(ss, se, si, sta[si]); // sets the value of children given parent si.
-        sta[si] = 0;
-    }
-
+ 
     void update(int qs, int qe, T val){ return update(0, N - 1, 0, qs, qe, val); }
     void update(int ss, int se, int si, int qs, int qe, T val){
-        propagate(ss, se, si);
         if(ss > qe || se < qs) return;
         if(qs <= ss && se <= qe){
-            stb[si] = true;
-            sta[si] = val;
-            propagate(ss, se, si);
+            st[si] = val * (se - ss + 1);
+            lazy[si] = val;
+            tag[si] = 1;
             return;
         }
+        push_down(ss, se, si);
         update(ss, MID(ss, se), L(si), qs, qe, val);
         update(MID(ss, se) + 1, se, R(si), qs, qe, val);
-        st[si] = join(st[L(si)], st[R(si)]);
+        st[si] = st[L(si)] + st[R(si)];
     }
-
+ 
     T get(int qs, int qe){ return get(0, N - 1, 0, qs, qe); }
     T get(int ss, int se, int si, int qs, int qe){
-        propagate(ss, se, si);
         if(qs <= ss && se <= qe) return st[si];
         if(ss > qe || se < qs) return NEUTRAL();
-        return join(get(ss, MID(ss, se), L(si), qs, qe), get(MID(ss, se) + 1, se, R(si), qs, qe));
+        push_down(ss, se, si);
+        return get(ss, MID(ss, se), L(si), qs, qe) + get(MID(ss, se) + 1, se, R(si), qs, qe);
     }
+ 
 };
--- data-structures/segment-tree.cpp
+++ data-structures/segment-tree.cpp
@@ -3,8 +3,7 @@
  * SEGMENT TREE.
  * O(log(N)) per query.
  * REQUIRED:
- *  - join
- *  - set_node
+ *  - merge
  *  - NEUTRAL
  * */
 
@@ -24,7 +23,7 @@ template<class T> struct SegmentTree{
     int MID(int ss, int se){ return (ss + se) / 2; }
     T NEUTRAL(){ return 0; }
  
-    T join(T lval, T rval){
+    T merge(T lval, T rval){
         return NEUTRAL();
     }
  
@@ -35,29 +34,23 @@ template<class T> struct SegmentTree{
         }
         build(ss, MID(ss, se), L(si), a);
         build(MID(ss, se) + 1, se, R(si), a);
-        st[si] = join(st[L(si)], st[R(si)]); 
+        st[si] = merge(st[L(si)], st[R(si)]); 
     }
  
     T get(int qs, int qe){ return get(0, N - 1, 0, qs, qe); }
     T get(int ss, int se, int si, int qs, int qe){
         if(qs <= ss && se <= qe) return st[si];
         if(ss > qe || se < qs) return NEUTRAL();
-        return join(get(ss, MID(ss, se), L(si), qs, qe), 
+        return merge(get(ss, MID(ss, se), L(si), qs, qe), 
                     get(MID(ss, se) + 1, se, R(si), qs, qe));   
-    }
-    
-    // sets new value of st[si] = node in update query
-    T set_node(T &node, T val){
-        // assign node
-        return node;
     }
  
     T update(int i, T val){ return update(0, N - 1, 0, i, val); }
     T update(int ss, int se, int si, int i, T val){
-        if(ss == se) return set_node(st[si], val);
-        if(i <= MID(ss, se)) return st[si] = join(update(ss, MID(ss, se), L(si), i, val), 
+        if(ss == se) return st[si] = val;
+        if(i <= MID(ss, se)) return st[si] = merge(update(ss, MID(ss, se), L(si), i, val), 
                                                   st[R(si)]);
-        else return st[si] = join(st[L(si)], update(MID(ss, se) + 1, se, R(si), i, val));
+        else return st[si] = merge(st[L(si)], update(MID(ss, se) + 1, se, R(si), i, val));
     }
     
 };
