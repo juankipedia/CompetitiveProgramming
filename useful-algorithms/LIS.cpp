151bab5bab0a6f9bd6753547030b7ccfcfc7aa30
juankipedia <juandiegp17@gmail.com>
2022-11-30 22:28:13 -0500

    solves LIS algorithm

--- useful-algorithms/LIS.cpp
+++ useful-algorithms/LIS.cpp
@@ -5,17 +5,13 @@
  *  if the sequence is not strictly grater change line 19 for using upper_bound instead of
  *  lower_bound and chance line 30 for using s[i] <= s[pos] instead of s[i] < s[pos].
  *  
- *  This problem could also be solved using a segment tree dp.
- *  For each element in s, find max(dp[s[i]], dp[s[i] - 1])
+ *  This problem could also be solved using a segment tree/bit dp.
+ *  For each element in s, find dp.set(s[i], max(dp(s[i], s[i]), dp(0, s[i] - 1) + 1))
  * 
  * */
 
-const int MAXN = 30000;
-int len, n;
-int s[MAXN], lis[MAXN];
-
-void LIS(){
-    len = 0;
+vector<int> lis(vector<int> &s){
+    int len = 0, n = s.size();
     vector<int> l;
     int lis_len[MAXN], pos;
     for(int i = 0; i < n; i++){
@@ -28,6 +24,7 @@ void LIS(){
             pos = i;
         }
     }
+    vector<int> lis(len);
     lis[len - 1] = s[pos];
     for(int i = pos - 1, j = len - 2; i >= 0; i--){
         if(s[i] < s[pos] && lis_len[i] == lis_len[pos] - 1){
@@ -36,4 +33,5 @@ void LIS(){
             j--;
         }
     }
+    return lis;
 }
