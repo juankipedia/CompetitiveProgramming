c2e460163df73880f8e5da3f72ae9a7b2a53eb7e
Juan Diego <juandiegp17@gmail.com>
2023-02-06 16:03:42 -0600

    new template

--- template.cpp
+++ template.cpp
@@ -1,13 +1,10 @@
 # include <bits/stdc++.h>
 using namespace std;
-/*************************************************************************************/
 # define endl "\n"
-# define io_boost std::ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-typedef unsigned long long int ulli;
-typedef long long int lli;
-/*************************************JUANKIPEDIA*************************************/
-
-int main(){
-
-    return 0;
-}
+# define io_boost std::ios_base::sync_with_stdio(0);cin.tie(0);
+using ull = unsigned long long;
+using ll = long long;
+/*
+    Que mira bobo ? 
+    JUANKI
+*/
