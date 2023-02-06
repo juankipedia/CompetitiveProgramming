ed3eb7824c29265addd933f454356d60571e9db8
Juan Diego <juandiegp17@gmail.com>
2023-02-06 16:04:55 -0600

    changes some stuff at the template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,7 @@
 # include <bits/stdc++.h>
 using namespace std;
 # define endl "\n"
-# define io_boost std::ios_base::sync_with_stdio(0);cin.tie(0);
+# define io_boost ios_base::sync_with_stdio(0);cin.tie(0);
 using ull = unsigned long long;
 using ll = long long;
 /*
