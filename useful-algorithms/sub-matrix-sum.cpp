717c2dfb8986cf8ef524c1d7f8c054d3a5107642
Juan Diego <juandiegp17@gmail.com>
2023-02-07 10:55:13 -0600

    Update sub-matrix-sum.cpp

--- useful-algorithms/sub-matrix-sum.cpp
+++ useful-algorithms/sub-matrix-sum.cpp
@@ -1,8 +1,6 @@
-const int MAXR = 1e3, MAXC = 1e3;
-
-int S[MAXR][MAXC], M[MAXR][MAXC], n, m;
- 
-void build(){
+template<class T> void build(vector<vector<T>> &M, vector<vector<T>> &S){
+    int n = M.size(), m = M[0].size();
+    S.assign(n, vector<T>(m, 0));
     for(int i = 0; i < m; i++) S[0][i] = M[0][i];
     for(int i = 1; i < n; i++)
         for(int j = 0; j < m; j++) S[i][j] = M[i][j] + S[i - 1][j];
@@ -11,7 +9,7 @@ void build(){
 }
 
 // get sum in the sub-matrix with oppsite vertices at (a, b), (c, d)
-int query(int a, int b, int c, int d){
+template<class T> int query(int a, int b, int c, int d, vector<vector<T>> &S){
     int res = S[c][d];
     if(a > 0) res = res - S[a - 1][d];
     if(b > 0) res = res - S[c][b - 1];
