e988ecaf8e6041befc87cd4f3dd174bfd90a7579
Juan Diego <juandiegp17@gmail.com>
2023-03-12 14:46:53 -0600

    Update kmp.cpp

--- strings/kmp.cpp
+++ strings/kmp.cpp
@@ -1,7 +1,9 @@
 /**
  *  KMP.
- *  Find all occurrences of p in txt (pattern searching).
- *  Time complexity: O(n)
+ * You are given a string s of length n. The prefix function for this string is defined as an array lps of length n, where 
+ * lps[i] is the length of the longest proper prefix of the substring s[0...i] which is also a suffix of this substring. 
+ * A proper prefix of a string is a prefix that is not equal to the string itself. By definition, lps[0] = 0.
+ * Time complexity: O(n)
  **/
 
 string txt, p;
