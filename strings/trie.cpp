e54f689ad248631085284ecb6865b8a78724c661
Juan Diego <juandiegp17@gmail.com>
2023-03-12 14:41:07 -0600

    Update trie.cpp

--- strings/trie.cpp
+++ strings/trie.cpp
@@ -9,29 +9,37 @@
  * 
  * 
  **/
+struct Trie{
+    
+    vector<bool> end_word;
+    int nodes = 1;
+    vector<vector<int>> trie;
 
-const int TRIE_LEN = 400005; // Dictionary size * length of words
- 
-int trie[TRIE_LEN][26], nodes = 1;
-bool end_word[TRIE_LEN];
-
-void add(string &s){
-    int cur = 0;
-    for(char c: s){
-        int cc = c - 'a';
-        if(!trie[cur][cc]) trie[cur][cc] = nodes++;
-        cur = trie[cur][cc];
+    Trie(int TRIE_LEN = 400005, int ALPHA_SIZE = 26){ // Dictionary size * length of words
+        end_word.assign(TRIE_LEN, 0);
+        trie.assign(TRIE_LEN, vector<int>(ALPHA_SIZE, 0));
     }
-    end_word[cur] = true;
-}
 
-bool check(string &t){
-    int cur = 0;
-    for(char c: t){
-        int cc = c - 'a';
-        cur = trie[cur][cc];
-        if(end_word[cur]) return true;
-        if(!cur) return false;
+    void add(const string &s){
+        int cur = 0;
+        for(char c: s){
+            int cc = c - 'a';
+            if(!trie[cur][cc]) trie[cur][cc] = nodes++;
+            cur = trie[cur][cc];
+        }
+        end_word[cur] = true;
     }
+
+    bool check(const string &t){
+        int cur = 0;
+        for(char c: t){
+            int cc = c - 'a';
+            cur = trie[cur][cc];
+            if(end_word[cur]) return true;
+            if(!cur) return false;
+        }
+        return false;
+    }
+};
     return false;
 }
